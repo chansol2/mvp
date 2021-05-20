ae570882e4bf940d7189c3b8b3a046e282491969
Chansol Lim <chansollim1994@berkeley.edu>
2021-05-20 15:42:23 +0900

    create a button toggle logic and apply it to the product-list-cafe.ejs file

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -3,3 +3,9 @@ $(".categoryClicked").on("click", function(event) {
   let val = $(name).attr("name");
   window.location.href = "/product/"+ val;
 });
+
+$(".medium-cate-tab").on("click", function(event) {
+  $(event.currentTarget).siblings().children().removeClass("active");
+  $(event.currentTarget).children().addClass("active");
+
+})
