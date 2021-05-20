b414b5c70e3244590bfa6ead3f5751beea9c795c
Chansol Lim <chansollim1994@berkeley.edu>
2021-05-20 19:29:31 +0900

    change the recommendation part's rendering logic and modify mobile toggle menu's design and routing logic

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -8,4 +8,16 @@ $(".medium-cate-tab").on("click", function(event) {
   $(event.currentTarget).siblings().children().removeClass("active");
   $(event.currentTarget).children().addClass("active");
 
+  target = $($(event.currentTarget).children().attr("name"));
+
+  sibs = target.siblings();
+  sibLen = sibs.length;
+  targetList = sibs.slice(1,sibLen);
+
+  targetList.addClass("d-none").removeClass("show");
+  target.removeClass("d-none").addClass("show");
+  // console.log(targetList)
+
+
+
 })
